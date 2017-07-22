b75e58a8798e172cc56e85d9fde12af4d9ad790f
Jamie <jagoda_blaut@hotmail.co.uk>
2017-07-22 20:40:17 +0100

    update path to JSON and readme

--- README.md
+++ README.md
@@ -11,4 +11,4 @@
 2. `docker build -t bbc_rmtask .`
 
 ## Usage
-1. `docker run -d --rm -p 8080:80 -v /($pwd)/app:/var/www/html bbc_rmtask`
+1. `docker run -d --rm -p 8080:80 -v /$(pwd)/app:/var/www/html bbc_rmtask`
--- app/server/response.php
+++ app/server/response.php
@@ -1,6 +1,6 @@
 <?php
 
-$service_url = 'http://192.168.99.100:8080/server/response.json';
+$service_url = $_SERVER['SERVER_NAME'] . '/server/response.json';
 $curl = curl_init($service_url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $curl_response = curl_exec($curl);
