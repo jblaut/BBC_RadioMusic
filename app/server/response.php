4e394a08662d77a3a1c2417cbfe7ca989b868fb8
Jamie <jagoda_blaut@hotmail.co.uk>
2017-07-22 20:30:35 +0100

    fix typo + update path to JSON file

--- app/server/response.php
+++ app/server/response.php
@@ -1,14 +1,14 @@
 <?php
 
-$service_url = 'http://localhost/work/autocomplete/app/server/response.json';
+$service_url = 'http://192.168.99.100:8080/server/response.json';
 $curl = curl_init($service_url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $curl_response = curl_exec($curl);
 if ($curl_response === false) {
     $info = curl_getinfo($curl);
     curl_close($curl);
-    die('error occured during curl exec. Additioanl info: ' . var_export($info));
-}
+    die('error occured during curl exec. Additional info: ' . var_export($info));
+} // CHANGE
 curl_close($curl);
 
 $results = json_decode($curl_response);
