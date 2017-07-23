f92bff9e163ee6dc1417b2cc6f5a619efab158eb
Jamie <jagoda_blaut@hotmail.co.uk>
2017-07-23 19:45:49 +0100

    add uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { rollup } from 'rollup';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify-es';
 
 export default {
   moduleName: 'foo',
@@ -9,6 +10,7 @@ export default {
   dest: 'app/scripts/bundle.js',
   plugins: [
     resolve({ jsnext: true, main: true }),
-    commonjs()
+    commonjs(),
+    uglify()
   ]
 };
