11cc3ff641818d79ebc6c61bba0409b5278078bd
Jamie <jagoda_blaut@hotmail.co.uk>
2017-07-23 02:11:41 +0100

    cleanup -> Retrieve class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,60 @@
 "use strict"
 
+class Retrieve {
+  constructor(path) {
+    this.path = path;
+    this.data = "";
+  }
+
+  filter(term, data) {
+    console.log(data)
+
+    let programmes = [];
+    for (let i = 0; i < data.length; i++) {
+      const titles = data[i]['programme']['title'];
+      const synopsis = data[i]['programme']['short_synopsis'];
+
+      if (titles.toLowerCase().search(term) >= 0) {
+        $('#noResults').hide();
+        if (data[i]['programme']['image'] != undefined &&
+            data[i]['programme']['image']['pid'] != undefined) {
+          const imageID = data[i]['programme']['image']['pid'];
+          let programme = new Programme(titles, synopsis, imageID);
+          programmes.push(programme.structure());
+        } else {
+          let programme = new Programme(titles, synopsis);
+          programmes.push(programme.structure());
+        }
+      }
+    }
+    return programmes;
+  }
+
+  search(searchTerm) {
+    if (this.data.length != 0) {
+      this.filter(searchTerm, this.data);
+    } else {
+      fetch(this.path).then(response => {
+        if (response.ok) {
+          return response;
+        }
+        return Promise.reject(Error('error'));
+      }).catch(error => {
+        return Promise.reject(Error(error.message));
+      })
+
+      .then(response => {
+        return response.json();
+      }).then(data => {
+        this.data = data;
+        this.filter(searchTerm, this.data);
+
+      })
+
+    }
+  }
+}
+
 class Programme {
   constructor(title, synopsis, pid=null) {
         this.title = title;
@@ -8,10 +63,12 @@ class Programme {
     }
 
     structure() {
-        let image = ""
+        let image = "";
+
         if (this.pid) {
           image = `<img src="https://ichef.bbci.co.uk/images/ic/480x270/${this.pid}.jpg">`;
         }
+
         return `<div class="programme">
                   ${image}
                   <div class="inner">
@@ -76,3 +133,6 @@ function keychange() {
 }
 
 $("#title").keyup(keychange);
+
+let retriever = new Retrieve("server/response.php");
+retriever.search("fry");
